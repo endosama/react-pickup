f28edec72ce7c5bf6e67e5087a44d357d6bd7ba0
Andrea Piras <piras.andrea.developer@gmail.com>
2019-11-06 19:06:36 +0100

    AC2 - input box with placeholder - imported node-sass

--- src/component/search/SearchWidget.tsx
+++ src/component/search/SearchWidget.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-
+import React, { useState, ChangeEvent } from 'react';
+import './SearchWidget.scss'
 
 export const SearchWidget: React.FC =  () => {
-    return <div style={styles.container}>
+    const [text, setText] = useState("")
+    const handleSetText = (e: ChangeEvent<HTMLInputElement>) => {
+        setText(e.currentTarget.value)
+    };
+ 
+    return <div className="SearchWidget__container">
         <h1>Where are we going?</h1>
-        <div style={styles.label}>Pick-up Location</div>
+        <div className="SearchWidget__label">Pick-up Location</div>
+        <input className="SearchWidget__pickup" value={text} onChange={handleSetText} type='text' placeholder='city, station, airport, region, distrinct...'/>
     </div>
 }
-
-const styles = ({
-    container: {
-        padding: 20,
-        backgroundColor: '#f3ce56'
-    },
-    label: {
-        fontWeight: 400
-    }
-})

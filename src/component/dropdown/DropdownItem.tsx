9ba9174b52a0f4a9091b73c69ee7840490cc4fac
Andrea Piras <piras.andrea.developer@gmail.com>
2019-11-06 23:09:22 +0100

    TEST2 - AC1 to AC5 - base implementation with styles

---
+++ src/component/dropdown/DropdownItem.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Location, LocationType } from '../../data/Location';
+import './DropdownItem.scss'
+
+interface IDropdownItem {
+    location: Location;
+    key: number
+}
+
+export const DropdownItem: React.FC<IDropdownItem> = (props) => {
+    const {location} = props;
+    let typeName: string
+    let typeColor: string
+    switch(location.type) {
+        case LocationType.Airport: 
+            typeName = 'Airport'
+            typeColor = 'red'
+            break;
+        case LocationType.Station: 
+            typeName = 'Station'
+            typeColor = 'gray'
+            break;
+        default: 
+            typeName = 'City'
+            typeColor = 'blue'
+            break;
+    }
+
+    return <div className='DropdownItem'>
+            <div className='DropdownItem__type'>
+                <span style={{backgroundColor: typeColor}}>{typeName}</span>
+            </div>
+            <div className='DropdownItem__content'>
+                <div className='DropdownItem__title'>
+                    <strong>{location.name}</strong>
+                    {location.iata ? <span>({location.iata})</span> : ''}
+                </div>
+                <div className='DropdownItem__subtitle'>
+                    <div>{location.region}</div>
+                </div>
+            </div>
+        </div>
+}
--- src/component/search/SearchWidget.tsx
+++ src/component/search/SearchWidget.tsx
@@ -1,15 +1,45 @@
 import React, { useState, ChangeEvent } from 'react';
 import './SearchWidget.scss'
+import { ResourceService } from '../../service/ResourceService';
+import { Location } from '../../data/Location';
+import { DropdownItem } from '../dropdown/DropdownItem';
+
+type DatasourceType = Location[] | null;
 
 export const SearchWidget: React.FC =  () => {
     const [text, setText] = useState("")
-    const handleSetText = (e: ChangeEvent<HTMLInputElement>) => {
-        setText(e.currentTarget.value)
+    const [loading, setLoading] = useState(false)
+    const [datasource, setDatasource] = useState<DatasourceType>(null)
+    const handleSetText = async (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.currentTarget.value
+        setText(value)
+        if(value && value.length > 1) {
+            setLoading(true)
+            const ds = await ResourceService.loadDatasource(value);
+            setDatasource(ds)
+            setLoading(false)
+        } else {
+            setDatasource(null)
+        }
     };
  
     return <div className="SearchWidget__container">
         <h1>Where are we going?</h1>
         <div className="SearchWidget__label">Pick-up Location</div>
-        <input className="SearchWidget__pickup" value={text} onChange={handleSetText} type='text' placeholder='city, station, airport, region, distrinct...'/>
+        <div>
+            <input className="SearchWidget__input" value={text} onChange={handleSetText} type='text' placeholder='city, station, airport, region, distrinct...'/>
+            {loading && <span>LOADING...</span>}
+        </div>
+        <div className="SearchWidget__pickup">
+            {
+                datasource &&
+                    (
+                        datasource.length > 0 ?
+                            datasource.map((location: Location, i: number) => (<DropdownItem key={i} location={location}></DropdownItem>))
+                        :
+                            <span>No results found</span>
+                    )
+            }
+        </div>
     </div>
 }

0028a037beea14b7144aa8f01f1535873de5a74f
Andrea Piras <piras.andrea.developer@gmail.com>
2019-11-08 00:23:14 +0100

    IE9 polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'react-app-polyfill/ie9';
 
 ReactDOM.render(<App />, document.getElementById('root'));
